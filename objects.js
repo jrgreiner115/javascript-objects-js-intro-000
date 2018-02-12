0b1c0d74b2242c5dc443633b4af931407959ee88
Joe Greiner <jrgreiner115@gmail.com>
2018-02-12 22:28:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = {artist : "song"}
 
-function updatePlaylist(playlist, artist, song) {Object
+function updatePlaylist(playlist, artist, song) {
+  Object.assign(playlist, {m83,:["Midnight City"]})
   
   
 }
