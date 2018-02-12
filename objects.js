e2dd4a016789372d17e11a8b292650f0399166f8
Joe Greiner <jrgreiner115@gmail.com>
2018-02-12 22:33:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ function updatePlaylist(playlist, artist, song) {
  playlist[artist] = song;
   return playlist;
 }
-function removeFromPlaylist(playlist,artist) {
-  delete playlist.artist;
+function removeFromPlaylist(playlist,artistname) {
+  delete playlist.artistname;
   return playlist
 }
